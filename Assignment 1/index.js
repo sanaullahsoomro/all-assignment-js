29ce3d2a724ccb16ed06d3ca77f560ebe74c4114
sanaullahsoomro <47575406+sanaullahsoomro@users.noreply.github.com>
2025-02-27 14:54:54 +0500

    Add files via upload

---
+++ Assignment 1/index.js
@@ -0,0 +1,92 @@
+//Assigment two (2)
+
+//Q#1  
+// var userame
+//  document.write("userame");
+
+ //Q#2
+//  var Myname= "Sanaullahsoomro"
+//  document.write(Myname);
+
+// Q#3
+// var message = prompt ("Enter massage");
+// alert(message);
+
+// Q#4
+// var Name= prompt("Name");
+// var id= prompt("id");
+// var loc= prompt("loc");
+// var Mobial= prompt("Mobial");
+ 
+// document.write( Name,"<br>" );
+// document.write(id ,"<br>") ;
+// document.write(loc , "<br>");
+// document.write(Mobial,"<br>");
+
+
+// Q#5
+// var one = prompt("Enter 1st");
+// var two = prompt("Enter 2nd");
+// var three = prompt("Enter 3rd");
+// var four = prompt("4th");
+// var five = prompt("5th");
+
+// alert(PIZZA);
+// alert(PIZZ);
+// alert(PIZ);
+// alert(PI);
+// alert(P);
+
+// document.write( one,"<br>",two,"<br>",three,"<br>",four,"<br>",five,"<br>");
+
+// Q#6
+// var Children = prompt("Enter the Number Of Children  ");
+// var partner = prompt("Enter the partner name  ");
+// var geographic = prompt("Enter the geographic location");
+// var job  = prompt("Enter the job tital  ");
+
+// document.write(Children , "<br>");
+// document.write(partner , "<br>");
+// document.write(geographic , "<br>");
+// document.write(job , "<br>");
+
+
+// Q#7
+// var email = prompt("Enter the Email");
+// alert("Your email address is " + email);
+
+// Q#8
+
+// let book = "A smarter way to learn JavaScript";
+// alert("I am reading the book: " + book);
+
+
+// Q#9
+// 
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// let university = prompt("Enter your UNI name ");
+// let course = prompt("Enter your course name");
+
+// alert(`My name is ${name}.
+// I am ${age} years old.
+// I study in ${university}.
+// I take ${course} classes.`);
+
+// #10
+// document.write("Yea! I can write HTML Content through Javascript");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment 1/index2.js
@@ -0,0 +1,26 @@
+
+        // // 1. Declare 3 variables in one statement
+        // var firstName = "John", lastName = "Doe", age = 25;
+
+        // // 2. Declare 5 legal & 5 illegal variable names
+
+        // // Legal variable names:
+        // var myVariable;
+        // var $price;
+        // var _name;
+        // var first_name;
+        // var myVar123;
+
+        //  Illegal variable names (Commented to prevent errors)
+        //  var 123name;   // Cannot start with a number
+        //  var my-variable; // Cannot contain hyphens
+        //  var var;        // Cannot use reserved keywords
+        //  var my variable; // Cannot contain spaces
+        //  var @data;      // Cannot contain special characters other than _ and $
+
+        //  3. Display variable naming rules using JavaScript
+        // document.getElementById("heading").innerText = "Rules for naming JS variables";
+        // document.getElementById("rule1").innerText = "Variable names can only contain letters, numbers, $ and _. For example: $my_1stVariable";
+        // document.getElementById("rule2").innerText = "Variables must begin with a letter, $ or _. For example: $name, _name, or name";
+        // document.getElementById("rule3").innerText = "Variable names are case-sensitive";
+        // document.getElementById("rule4").innerText = "Variable names should not be JS reserved keywords";
